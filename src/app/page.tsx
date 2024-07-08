fd299c0cd2b81833f3299e191b220a844665b943
Dilpreet-Singh-Sandhu <dilpreetsandhuxx12@gmail.com>
2024-07-08 18:45:58 +0530

    finally Done

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import FeaturedCourses from "@/components/FeaturedCourses"
+import HeroSection from "@/components/HeroSection"
+import Instructors from "@/components/Instructors"
+import TestimonialCards from "@/components/TestimonialCards"
+import UpComingWebinars from "@/components/UpComingWebinars"
+import WhyChooseUs from "@/components/WhyChooseUs"
+import Footer from "@/components/footer"
+
+export default function Home(){
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+      <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+      <TestimonialCards/>
+      <UpComingWebinars/>
+      <Instructors/>
+      <Footer/>
+    </main>
+  )
+}
+
